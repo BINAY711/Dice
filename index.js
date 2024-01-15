989795ac175b2dcfb42ee04b569432ace33e871a
Binay Lama <binaylama06@gmail.com>
2024-01-15 18:11:18 +0545

    complete

---
+++ index.js
@@ -0,0 +1,85 @@
+
+//document.querySelector(".roll1").addEventListener("click",function(){
+
+
+function rollDice1(){  
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+if ((randomNumber1 + randomNumber2)>7) {
+  document.querySelector("h1").innerHTML ="you win"
+}
+
+else {
+  document.querySelector("h1").innerHTML = "you lose";
+}
+}
+
+
+// document.querySelector(".roll2").addEventListener("click",function(){
+
+
+function rollDice2(){ 
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+if ((randomNumber1+randomNumber2)<7) {
+  document.querySelector("h1").innerHTML = "you win";
+}
+
+else {
+  document.querySelector("h1").innerHTML = "you lose";
+}
+}
+function rollDice3(){ 
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+  if ((randomNumber1+randomNumber2)==7) {
+    document.querySelector("h1").innerHTML = "you win";
+  }
+  
+  else {
+    document.querySelector("h1").innerHTML = "you lose";
+  }
+}
